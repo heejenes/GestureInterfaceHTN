c18ed98faed794287d585f315535e83622330a44
MrBruh <mdnssv@gmail.com>
2019-09-14 13:50:07 -0400

    Switched to background differential and code clean up

--- hands.py
+++ hands.py
@@ -1,21 +1,14 @@
-# USAGE
-# python openvino_real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt --model MobileNetSSD_deploy.caffemodel
 
-# import the necessary packages
 from imutils.video import VideoStream
 from imutils.video import FPS
 from picamera import PiCamera
 import numpy as np
-import argparse
 import imutils
 import time
 import cv2
 import math
-import serial
-import argparse
 import time
 import random
-
 
 
 vs = VideoStream(usePiCamera=True).start()
@@ -25,9 +18,45 @@ vs.camera.awb_mode = 'off'
 vs.camera.awb_gains = (1.5, 1.5)
 vs.camera.framerate = 20
 fps = FPS().start()
+bg = None
 
 def nothing(x):
 	pass
+
+#--------------------------------------------------
+# To find the running average over the background
+#--------------------------------------------------
+def run_avg(image, aWeight):
+    global bg
+    # initialize the background
+    if bg is None:
+        bg = image.copy().astype("float")
+        return
+
+    # compute weighted average, accumulate it and update the background
+    cv2.accumulateWeighted(image, bg, aWeight)
+
+#---------------------------------------------
+# To segment the region of hand in the image
+#---------------------------------------------
+def segment(image, threshold=25):
+    global bg
+    # find the absolute difference between background and current frame
+    diff = cv2.absdiff(bg.astype("uint8"), image)
+
+    # threshold the diff image so that we get the foreground
+    thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
+
+    # get the contours in the thresholded image
+    (cnts, _) = cv2.findContours(thresholded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # return None, if no contours detected
+    if len(cnts) == 0:
+        return
+    else:
+        # based on contour area, get the maximum contour which is the hand
+        segmented = max(cnts, key=cv2.contourArea)
+        return (thresholded, segmented)
 
 def assign_trackbar(window):
 	Blur = cv2.getTrackbarPos("Blur", window)
@@ -51,22 +80,6 @@ cv2.createTrackbar("rg1", "awb", 12, 80, nothing)
 cv2.createTrackbar("iso", "awb", 800, 800, nothing)
 cv2.createTrackbar("comp", "awb", 25, 50, nothing)
 
-#arguments for # of lanes to follow
-parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--lanes", type=int, default=0, choices=range(0, 3),
-                    help="The number of games to simulate")
-args = parser.parse_args()
-lane_follow_option = args.lanes
-#print(lane_follow_option)	
-
-steer_angle = 9
-time.sleep(2.0)
-white_offset = 0
-yellow_offset = 0
-offset_count = 0
-offset_added = False
-#writesteer(steer_angle)
-#frame = cv2.imread('1.jpg')
 s = 400
 #frame = cv2.resize(frame, (s,s), 0, 0, cv2.INTER_AREA)
 #hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -76,20 +89,14 @@ frame_count = 0
 # loop over the frames from the video stream
 print("succeeded")
 print(vs.camera.resolution)
-race_almost_started = False
-race_primed = False
-race_started = False
-race_started_count = 0
-race_ended = False
-motor_started = False
-motor_braked = False
-direction_chosen = False
-direction_count = 0
+aWeight = 0.5
+hand = None
+
 while True:   
 
-	bg = (cv2.getTrackbarPos("bg1", "awb")/10)#, cv2.getTrackbarPos("bg2", "awb")) 
+	bluegain = (cv2.getTrackbarPos("bg1", "awb")/10)#, cv2.getTrackbarPos("bg2", "awb")) 
 	rg = (cv2.getTrackbarPos("rg1", "awb")/10)#, cv2.getTrackbarPos("rg2", "awb"))
-	vs.camera.awb_gains = (rg, bg)
+	vs.camera.awb_gains = (rg, bluegain)
 	iso = cv2.getTrackbarPos("iso", "awb")
 	vs.camera.iso = iso
 	exposure = cv2.getTrackbarPos("comp", "awb") - 25
@@ -99,102 +106,119 @@ while True:
 	#	guassian += 1
 	frame = vs.read()
 	#frame = cv2.imread('1.jpg')
-	frame = cv2.resize(frame, (s,s), 0, 0, cv2.INTER_AREA)
-	lines_edges = frame 
+	#frame = cv2.resize(frame, (s,s), 0, 0, cv2.INTER_AREA)
+	lines_edges = frame
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
 	#hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 	#blur = cv2.GaussianBlur(hsv,(guassian,guassian),0)
 
-	#red_params = assign_trackbar("Red")
-	#magenta_params = assign_trackbar("Magenta")
-	#white_params = assign_trackbar("White")
-	
 	#TAEHOON########################################
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	#hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 	#background color is [255,255,255]
 	
-	upper = np.array([20,255,255])
-	lower = np.array([0,48,80])
+	#upper = np.array([20,255,255])
+	#lower = np.array([0,48,80])
 	#generates mask
-	colorMask = cv2.inRange(hsv,lower,upper)
-	
-	colorMask = cv2.medianBlur(colorMask, 3)
-	
-	#generates contour
-	cont, a = cv2.findContours(colorMask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-	#print(len(cont))
-	if len(cont)>0:
-		longest = 0
-		for i in range(len(cont)):
-			if len(cont[longest])<len(cont[i]):
-				longest = i
-		mainCont = cont[longest]
-		#print("mainCont: ",mainCont)
-		mainWCont = frame.copy()
-		cv2.drawContours(mainWCont, [mainCont], 0, (0,255,0),3)
-	
-		#generates hull
-		hullIndex = cv2.convexHull(mainCont, returnPoints = False)
-		hull = []
-		[hull.append(mainCont[i][0]) for i in hullIndex]
-		#print("hull is: ",hull[0])
+	#colorMask = cv2.inRange(hsv,lower,upper)
+
+	if frame_count < 30:
+		run_avg(gray, aWeight)
+	else:
+			# segment the hand region
+			hand = segment(gray)
+
+			# check whether hand region is segmented
+			if hand is not None:
+				# if yes, unpack the thresholded image and
+				# segmented region
+				(thresholded, segmented) = hand
+
+				# draw the segmented region and display the frame
+				#cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
+				#cv2.imshow("Thesholded", thresholded)
+
+	if hand is not None:
+		(thresholded, segmented) = hand
+		thresholded = cv2.medianBlur(thresholded, 3)
 		
-		#general functions
-		def dist(pointA,pointB):
-			A=pointA[0]
-			B=pointB[0]
-			a=((A[0]-B[0])**2+(A[1]-B[1])**2)**0.5
-			#print(a)
-			return a
-		def average(points):
-			a=0
-			b=0
-			#print(len(points),"points: ",points)
-			for i in range(len(points)):
-				a+=points[i][0][0]
-				b+=points[i][0][1]
-			return [int(round(a/len(points))),int(round(b/len(points)))]
-		#groups hull
-		maxDist = 25
-		finalHull = []
-		for i in range(len(hull)):
-			group = [hull[i]]
-			for j in range(1,len(hull)):
-				a=i+j
-				if i+j >= len(hull):
-					a = i+j-len(hull)
-				if dist(hull[a-1],hull[a])<=maxDist:
-					group.append(hull[a])
-				else:
-					break
-						
-			for j in range(1,len(hull)):
-				a=i-j
-				if dist(hull[a+1],hull[a])<=maxDist:
-					group.append(hull[a])
-				else:
-					break
-			if [average(group)] in finalHull:
-				#print("skipped")
-				continue
-			finalHull.append([average(group)])
-		#print("finalhull is: ", finalHull)
-		[cv2.circle(mainWCont, tuple(curPoint[0]),20,(0,0,255),5) for curPoint in finalHull]
+		#generates contour
+		cont, a = cv2.findContours(thresholded ,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+		#print(len(cont))
+		if len(cont)>0:
+			longest = 0
+			for i in range(len(cont)):
+				if len(cont[longest])<len(cont[i]):
+					longest = i
+			mainCont = cont[longest]
+			#print("mainCont: ",mainCont)
+			mainWCont = frame.copy()
+			cv2.drawContours(mainWCont, [mainCont], 0, (0,255,0),3)
 		
-		
-		cv2.drawContours(mainWCont, [np.array(finalHull)], 0, (0,255,0),3)
-		
-		cv2.imshow("Cont", mainWCont)
-		
-		
-		#convexity defects
-		defects = cv2.convexityDefects(mainCont, hullIndex)
-		if type(defects) is np.ndarray:
-			print(defects[0][0][2])
-			print(mainCont[defects[0][0][2]][0])
-			[cv2.circle(mainWCont, tuple(mainCont[curPoint[0][2]][0]),20,(0,0,255),5) for curPoint in defects]
+			#generates hull
+			hullIndex = cv2.convexHull(mainCont, returnPoints = False)
+			hull = []
+			[hull.append(mainCont[i][0]) for i in hullIndex]
+			#print("hull is: ",hull[0])
+			
+			#general functions
+			def dist(pointA,pointB):
+				A=pointA[0]
+				B=pointB[0]
+				a=((A[0]-B[0])**2+(A[1]-B[1])**2)**0.5
+				#print(a)
+				return a
+			def average(points):
+				a=0
+				b=0
+				#print(len(points),"points: ",points)
+				for i in range(len(points)):
+					a+=points[i][0][0]
+					b+=points[i][0][1]
+				return [int(round(a/len(points))),int(round(b/len(points)))]
+			#groups hull
+			maxDist = 25
+			finalHull = []
+			for i in range(len(hull)):
+				group = [hull[i]]
+				for j in range(1,len(hull)):
+					a=i+j
+					if i+j >= len(hull):
+						a = i+j-len(hull)
+					if dist(hull[a-1],hull[a])<=maxDist:
+						group.append(hull[a])
+					else:
+						break
+							
+				for j in range(1,len(hull)):
+					a=i-j
+					if dist(hull[a+1],hull[a])<=maxDist:
+						group.append(hull[a])
+					else:
+						break
+				if [average(group)] in finalHull:
+					#print("skipped")
+					continue
+				finalHull.append([average(group)])
+			#print("finalhull is: ", finalHull)
+			[cv2.circle(mainWCont, tuple(curPoint[0]),20,(0,0,255),5) for curPoint in finalHull]
+			
+			
+			cv2.drawContours(mainWCont, [np.array(finalHull)], 0, (0,255,0),3)
+			
+			cv2.imshow("Cont", mainWCont)
+			
+			
+			#convexity defects
+			defects = cv2.convexityDefects(mainCont, hullIndex)
+			if type(defects) is np.ndarray:
+				print(defects[0][0][2])
+				print(mainCont[defects[0][0][2]][0])
+				[cv2.circle(mainWCont, tuple(mainCont[curPoint[0][2]][0]),20,(0,0,255),5) for curPoint in defects]
 
 	cv2.imshow("Main", lines_edges)
-	cv2.imshow("Mask", colorMask)
+	if hand is not None:
+		cv2.imshow("Mask", thresholded)
 	
 	key = cv2.waitKey(1) & 0xFF
 
@@ -204,7 +228,9 @@ while True:
 
 	# update the FPS counter
 	fps.update()
-	#time.sleep(0.02)
+
+	frame_count += 1
+	
 
 # stop the timer and display FPS information
 fps.stop()
